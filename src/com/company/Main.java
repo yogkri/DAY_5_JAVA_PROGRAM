a69e19f63e95c9f0759d9223225bbba40be800c9
Yogendra <dharmayog91@gmail.com>
2021-09-09 19:17:00 +0530

    CHECK WEATHER THE PROGRAM HAS LARGEST NUMBER

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -41,17 +41,24 @@ public class Main {
                 System.out.println(character + " : is an Consonent");
 
 
-
+                int number1, number2, number3;
+                System.out.println("We have to check largest among these three numbers below ");
                 Scanner sc = new Scanner(System.in);
-                System.out.print("Enter a number to check even or odd : ");
-                int num = sc.nextInt();
-                if (num % 2 == 0){
-                    System.out.println(num + " : is an even number");
+                System.out.print("Enter First number : ");
+                number1 = sc.nextInt();
+                System.out.print("Enter Second number : ");
+                number2 = sc.nextInt();
+                System.out.print("Enter Third number : ");
+                number3 = sc.nextInt();
+                //
+                if (number1 > number2 && number1 > number3) {
+                    System.out.println(number1 + "  is largest in " + number1 + "," + number2 + "," + number3);
+                } else if (number2 > number1 && number2 > number3) {
+                    System.out.println(number2 + "  is largest in " + number1 + "," + number2 + "," + number3);
+                } else {
+                    System.out.println(number3 + "  is largest in " + number1 + "," + number2 + "," + number3);
                 }
-                else {
-                    System.out.println(num + " : is an odd number");
-                }
-            }
         }
     }
+}
 
