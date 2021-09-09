17d54067b8764b21e0f3e7681e15feb9f8ded019
Yogendra <dharmayog91@gmail.com>
2021-09-09 18:57:49 +0530

    SWAPPING OF TWO NUMBERS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+            int a, b;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the value of a : ");
+            a = sc.nextInt();
+            System.out.print("Enter the value of b : ");
+            b = sc.nextInt();
+            int t = 0;
 
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Input a number: ");
-            int number = scan.nextInt();
-            if (number > 0) {
-                while (number % 2 == 0) {
-                    System.out.print(2 + " ");
-                    number /= 2;
-                }
+            t = a;
+            a = b;
+            b = t;
 
-                for (int i = 3; i <= Math.sqrt(number); i += 2) {
-                    while (number % i == 0) {
-                        System.out.print(i + " ");
-                        number /= i;
-                    }
-                }
-                if (number > 2)
-                    System.out.print(number);
-            }
             int quotient, remainder, dividend, divisor;
             Scanner scanner = new Scanner(System.in);
 
@@ -36,6 +29,8 @@ public class Main {
             remainder = dividend % divisor;
             System.out.println("Quotient = " + quotient);
             System.out.println("Remainder= " + remainder);
+        System.out.println("The swap value of a is : " + a);
+        System.out.println("The swap value of b is : " + b);
         }
     }
 
