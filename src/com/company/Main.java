bb31379a0d224d2f4ad60c86d1eefbceeab42cfb
Yogendra <dharmayog91@gmail.com>
2021-09-09 19:05:21 +0530

    CHECK WEATHER THE PROGRAM IS EVEN OR ODD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,34 +3,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a : ");
+        a = sc.nextInt();
+        System.out.print("Enter the value of b : ");
+        b = sc.nextInt();
+        int t = 0;
 
-            int a, b;
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter the value of a : ");
-            a = sc.nextInt();
-            System.out.print("Enter the value of b : ");
-            b = sc.nextInt();
-            int t = 0;
+        t = a;
+        a = b;
+        b = t;
 
-            t = a;
-            a = b;
-            b = t;
-
-            int quotient, remainder, dividend, divisor;
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Enter Dividend value : ");
-            dividend = scanner.nextInt();
-
-            System.out.print("Enter Divisor value : ");
-            divisor = scanner.nextInt();
-
-            quotient = dividend / divisor;
-            remainder = dividend % divisor;
-            System.out.println("Quotient = " + quotient);
-            System.out.println("Remainder= " + remainder);
         System.out.println("The swap value of a is : " + a);
         System.out.println("The swap value of b is : " + b);
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter a number to check even or odd : ");
+        int num = sc1.nextInt();
+        if (num % 2 == 0){
+            System.out.println(num + " : is an even number");
+        }
+        else {
+            System.out.println(num + " : is an odd number");
         }
     }
+}
 
