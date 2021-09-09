a6ce4d6e871fd5c9978eef120c9776cb50ac6670
Yogendra <dharmayog91@gmail.com>
2021-09-09 18:46:06 +0530

    HARMONC NUMBER

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,9 @@
 package com.company;
 import java.util.Scanner;
-
-
 public class Main {
 
-    static final int MAX_VALUE = 32;
+
+    static final int MAX_VALUE = 64;
     static final int MIN_VALUE = 0;
 
     public static void main(String[] args) {
@@ -19,10 +18,24 @@ public class Main {
             for (int i = 0; i < number; i++) {
                 power = (int) Math.pow(2, i);
                 System.out.println("2" + "^" + i + "=" + power);
+               {
+
+                    Float harmonicNumber = 1f;
+                    int number1;
+                    System.out.print("Enter the number to Check Harmonic expression (1+ 1/2 + 1/3 + ....+ 1/N) : ");
+                    Scanner scanner1 = new Scanner(System.in);
+                    number = scanner.nextInt();
+
+                    if (number != 0) {
+                        for (int i1 = 2; i <= number; i++) {
+                            System.out.println("i value:" + i);
+                            harmonicNumber += (float) 1 / i;
+                            System.out.println(harmonicNumber);
+                        }
+                        System.out.print("Harmonic Value of Number " + number + " is :  " + harmonicNumber);
+                    }
+                }
             }
         }
     }
 }
-
-
-
