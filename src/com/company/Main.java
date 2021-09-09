da48b983421b735fb1995c383a0e7d86b573724a
Yogendra <dharmayog91@gmail.com>
2021-09-09 19:40:35 +0530

    QUADRATIC

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,39 +2,32 @@ package com.company;
 import java.util.Scanner;
 import java.util.Scanner;
 
-
-class Main
+public class Main
 {
-    public static void main(String arg[])
-
+    public static void main(String[] Strings)
     {
-
-        int x1,x2,y1,y2;
-
-        double dis;
-
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("enter x1 point");
-
-        x1=sc.nextInt();
-
-        System.out.println("enter y1 point");
-
-        y1=sc.nextInt();
-
-        System.out.println("enter x2point");
-
-        x2=sc.nextInt();
-
-        System.out.println("enter y2 point");
-
-        y2=sc.nextInt();
-
-        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
-        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter the value of b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter the value of c: ");
+        double c = input.nextDouble();
+        double d= b * b - 4.0 * a * c;
+        if (d> 0.0)
+        {
+            double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+            System.out.println("The roots are " + r1 + " and " + r2);
+        }
+        else if (d == 0.0)
+        {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        }
+        else
+        {
+            System.out.println("Roots are not real.");
+        }
     }
-
 }
