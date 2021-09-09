cf00a0592693a5473f62aab20b9cf074f8093a5d
Yogendra <dharmayog91@gmail.com>
2021-09-09 18:50:25 +0530

    PRIME FACTORS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,40 +2,40 @@ package com.company;
 import java.util.Scanner;
 public class Main {
 
-
-    static final int MAX_VALUE = 64;
-    static final int MIN_VALUE = 0;
-
     public static void main(String[] args) {
 
+        Float harmonicNumber = 1f;
         int number;
-        int power;
-        System.out.print("Enter a number to Print 2 power Table : ");
+        System.out.print("Enter the number to Check Harmonic expression (1+ 1/2 + 1/3 + ....+ 1/N) : ");
         Scanner scanner = new Scanner(System.in);
         number = scanner.nextInt();
 
-        if (number >= MIN_VALUE && number < MAX_VALUE) {
-            for (int i = 0; i < number; i++) {
-                power = (int) Math.pow(2, i);
-                System.out.println("2" + "^" + i + "=" + power);
-               {
+        if (number != 0) {
+            for (int i = 2; i <= number; i++) {
+                System.out.println("i value:" + i);
+                harmonicNumber += (float) 1 / i;
+                System.out.println(harmonicNumber);
 
-                    Float harmonicNumber = 1f;
-                    int number1;
-                    System.out.print("Enter the number to Check Harmonic expression (1+ 1/2 + 1/3 + ....+ 1/N) : ");
-                    Scanner scanner1 = new Scanner(System.in);
-                    number = scanner.nextInt();
-
-                    if (number != 0) {
-                        for (int i1 = 2; i <= number; i++) {
-                            System.out.println("i value:" + i);
-                            harmonicNumber += (float) 1 / i;
-                            System.out.println(harmonicNumber);
-                        }
-                        System.out.print("Harmonic Value of Number " + number + " is :  " + harmonicNumber);
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Input a number: ");
+                int number1 = scan.nextInt();
+                if (number > 0) {
+                    while (number % 2 == 0) {
+                        System.out.print(2 + " ");
+                        number /= 2;
                     }
+
+                    for (int i1 = 3; i <= Math.sqrt(number); i += 2) {
+                        while (number % i == 0) {
+                            System.out.print(i + " ");
+                            number /= i;
+                        }
+                    }
+                    if (number > 2)
+                        System.out.print(number);
                 }
             }
+            System.out.print("Harmonic Value of Number " + number + " is :  " + harmonicNumber);
         }
     }
 }
