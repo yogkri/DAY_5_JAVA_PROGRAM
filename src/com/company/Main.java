58ab33eaeac49c67dc097d6be7f2b6a4c3466e31
Yogendra <dharmayog91@gmail.com>
2021-09-09 18:38:46 +0530

    POWER OF TWO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,28 @@
 package com.company;
-
-
 import java.util.Scanner;
+
+
 public class Main {
 
-    static final int HEADFLIP = 1;
-    static final int COUNT = 100;
+    static final int MAX_VALUE = 32;
+    static final int MIN_VALUE = 0;
 
     public static void main(String[] args) {
 
+        int number;
+        int power;
+        System.out.print("Enter a number to Print 2 power Table : ");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
 
-                String leapYearCheck;
-                System.out.print("Enter Year to Check it LeapYear or not : ");
-                Scanner scanner = new Scanner(System.in);
-                leapYearCheck = scanner.nextLine();
-
-                if (leapYearCheck.length() == 4) {
-
-                    int leapYear = Integer.parseInt(leapYearCheck);
-                    System.out.println("Entred year is : " + leapYear);
-                    if ((leapYear % 4) == 0 && (leapYear % 100) != 0 || (leapYear % 400) == 0) {
-                        System.out.println(leapYear + " is a LeapYear");
-                    } else {
-                        System.out.println(leapYear + " is not a LeapYear");
-                    }
-
-                } else {
-                    System.out.println("Invalid Year! Enter an valid Year of 4 digit ");
-                }
+        if (number >= MIN_VALUE && number < MAX_VALUE) {
+            for (int i = 0; i < number; i++) {
+                power = (int) Math.pow(2, i);
+                System.out.println("2" + "^" + i + "=" + power);
             }
         }
+    }
+}
 
 
 
