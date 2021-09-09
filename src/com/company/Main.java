724cb4906adfab4cb465c22a22bfd8d6f193ef53
Yogendra <dharmayog91@gmail.com>
2021-09-09 19:13:44 +0530

    CHECK WEATHER THE PROGRAM IS CONSONENT OR VOWEL

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,57 @@
 package com.company;
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        int a, b;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of a : ");
-        a = sc.nextInt();
-        System.out.print("Enter the value of b : ");
-        b = sc.nextInt();
-        int t = 0;
+        char character;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Character : ");
+        character = scanner.next().charAt(0);
+        switch (character) {
+            case 'a':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'e':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'i':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'o':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'u':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'A':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'E':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'I':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'O':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'U':
+                System.out.println(character + " + : is an Vowel");
+                break;
+            default:
+                System.out.println(character + " : is an Consonent");
 
-        t = a;
-        a = b;
-        b = t;
 
-        System.out.println("The swap value of a is : " + a);
-        System.out.println("The swap value of b is : " + b);
 
-        Scanner sc1 = new Scanner(System.in);
-        System.out.print("Enter a number to check even or odd : ");
-        int num = sc1.nextInt();
-        if (num % 2 == 0){
-            System.out.println(num + " : is an even number");
-        }
-        else {
-            System.out.println(num + " : is an odd number");
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter a number to check even or odd : ");
+                int num = sc.nextInt();
+                if (num % 2 == 0){
+                    System.out.println(num + " : is an even number");
+                }
+                else {
+                    System.out.println(num + " : is an odd number");
+                }
+            }
         }
     }
-}
 
