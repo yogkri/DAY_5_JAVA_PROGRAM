1f4cc669841723f1a825c14871958d79b4d2af0e
Yogendra <dharmayog91@gmail.com>
2021-09-09 18:53:25 +0530

    PROGRAM FOR QUITENT AND REMAINDER

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,38 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Float harmonicNumber = 1f;
-        int number;
-        System.out.print("Enter the number to Check Harmonic expression (1+ 1/2 + 1/3 + ....+ 1/N) : ");
-        Scanner scanner = new Scanner(System.in);
-        number = scanner.nextInt();
 
-        if (number != 0) {
-            for (int i = 2; i <= number; i++) {
-                System.out.println("i value:" + i);
-                harmonicNumber += (float) 1 / i;
-                System.out.println(harmonicNumber);
-
-                Scanner scan = new Scanner(System.in);
-                System.out.print("Input a number: ");
-                int number1 = scan.nextInt();
-                if (number > 0) {
-                    while (number % 2 == 0) {
-                        System.out.print(2 + " ");
-                        number /= 2;
-                    }
-
-                    for (int i1 = 3; i <= Math.sqrt(number); i += 2) {
-                        while (number % i == 0) {
-                            System.out.print(i + " ");
-                            number /= i;
-                        }
-                    }
-                    if (number > 2)
-                        System.out.print(number);
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Input a number: ");
+            int number = scan.nextInt();
+            if (number > 0) {
+                while (number % 2 == 0) {
+                    System.out.print(2 + " ");
+                    number /= 2;
                 }
+
+                for (int i = 3; i <= Math.sqrt(number); i += 2) {
+                    while (number % i == 0) {
+                        System.out.print(i + " ");
+                        number /= i;
+                    }
+                }
+                if (number > 2)
+                    System.out.print(number);
             }
-            System.out.print("Harmonic Value of Number " + number + " is :  " + harmonicNumber);
+            int quotient, remainder, dividend, divisor;
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter Dividend value : ");
+            dividend = scanner.nextInt();
+
+            System.out.print("Enter Divisor value : ");
+            divisor = scanner.nextInt();
+
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+            System.out.println("Quotient = " + quotient);
+            System.out.println("Remainder= " + remainder);
         }
     }
-}
+
